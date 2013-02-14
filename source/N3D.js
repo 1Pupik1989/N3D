4135a6cdf117895c36d9c642d2fc61002145d4bf
1Pupik1989 <pupik.heuripitec@centrum.cz>
2013-02-14 20:39:32 +0100

    add multiply two Matrices 4x4

--- source/N3D.js
+++ source/N3D.js
@@ -111,6 +111,41 @@ N3D.Matrix4.prototype = {
   }
 };
 
+N3D.Matrix4.Multiply = function(m,n){
+    var m0 = m.m[0], m1 = m.m[1], m2 = m.m[2], m3 = m.m[3],
+        m4 = m.m[4], m5 = m.m[5], m6 = m.m[6], m7 = m.m[7],
+        m8 = m.m[8], m9 = m.m[9], m10 = m.m[10], m11 = m.m[11],
+        m12 = m.m[12], m13 = m.m[13], m14 = m.m[14], m15 = m.m[15];
+        
+    var n0 = n.m[0], n1 = n.m[1], n2 = n.m[2], n3 = n.m[3],
+        n4 = n.m[4], n5 = n.m[5], n6 = n.m[6], n7 = n.m[7],
+        n8 = n.m[8], n9 = n.m[9], n10 = n.m[10], n11 = n.m[11],
+        n12 = n.m[12], n13 = n.m[13], n14 = n.m[14], n15 = n.m[15];
+        
+    return new N3D.Matrix4(
+      m0*n0 + m1*n4 + m2*n8 + m3*n12,
+      m0*n1 + m1*n5 + m2*n9 + m3*n13,
+      m0*n2 + m1*n6 + m2*n10 + m3*n14,
+      m0*n3 + m1*n7 + m2*n11 + m3*n15,
+      
+      m4*n0 + m5*n4 + m6*n8 + m7*n12,
+      m4*n1 + m5*n5 + m6*n9 + m7*n13,
+      m4*n2 + m5*n6 + m6*n10 + m7*n14,
+      m4*n3 + m5*n7 + m6*n11 + m7*n15,
+      
+      m8*n0 + m9*n4 + m10*n8 + m11*n12,
+      m8*n1 + m9*n5 + m10*n9 + m11*n13,
+      m8*n2 + m9*n6 + m10*n10 + m11*n14,
+      m8*n3 + m9*n7 + m10*n11 + m11*n15,
+      
+      m12*n0 + m13*n4 + m14*n8 + m15*n12,
+      m12*n1 + m13*n5 + m14*n9 + m15*n13,
+      m12*n2 + m13*n6 + m14*n10 + m15*n14,
+      m12*n3 + m13*n7 + m14*n11 + m15*n15      
+    );
+     
+};
+
 
 N3D.Vector3 = function(x,y,z){
   this.x = x;
